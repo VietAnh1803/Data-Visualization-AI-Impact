6d4ef57897534d16ca6ca58a1167d0ababaa5d44
Nguyễn Việt Anh <137848364+VietAnh1803@users.noreply.github.com>
2025-06-19 10:07:01 +0700

    Update analysisfilecsv.py

--- analysisfilecsv.py
+++ analysisfilecsv.py
@@ -1,4 +1,4 @@
 import pandas as pd
-file_path = 'C:\Hoc_tap\Data\DA\Global_AI_Content_Impact_Dataset.csv'
+file_path = '...\Global_AI_Content_Impact_Dataset.csv'
 df = pd.read_csv(file_path)
 print(df.head())
